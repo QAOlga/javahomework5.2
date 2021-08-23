facab08fcc82e3320cec51c5e5fd3f55559c6b89
OlgaMikhaylyukova <olele@inbox.ru>
2021-08-23 22:42:21 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
+        // задаем ожидаемое значение
+        long expected = 30;
 
-    service.calculate(amount, registered);
-  }
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+        // сравниваем ожидаемый и фактический результат
+        boolean passed = expected == actual;
+
+        // выводим результат
+        System.out.println(passed);
+    }
 }
 
